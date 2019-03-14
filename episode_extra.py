f1d82e9ad6f8fc4a0d6efb731480b90e76c5e797
LeoLiu1998 <zeyuliu2@uw.edu>
2019-03-13 21:16:14 -0700

    draft extra

--- episode_extra.py
+++ episode_extra.py
@@ -33,8 +33,8 @@ class Episode:
         self.objects = int_objects + rec_objects
         self.actions_list = [{'action': a} for a in BASIC_ACTIONS]
         self.actions_taken = []
-        self.done_each_action = [0, 0, 0, 0]  # store agents' judgements
-        self.successes = [0, 0, 0, 0]
+        self.done_each_action = [0, 0]  # store agents' judgements
+        self.successes = [0, 0]
         # self.seen_objects = [0 for _ in range(len(self.objects))]
         self.success = False
         self.distances = [float('inf'), float('inf')]
@@ -78,42 +78,55 @@ class Episode:
         # immediate reward
         reward = STEP_PENALTY
 
-        all_done = False
+        # all_done = False
 
         action_was_successful = self.environment.last_action_success
         if self.args.improve:
             objects = self._env.last_event.metadata['objects']
             agent_pos = self._env.last_event.metadata['agent']['position']
-        
+
             visible_objects = [o['objectType'] for o in objects if o['visible']]
             for i in range(len(self.distances)):
                 target = self.target[i]
-                if target in visible_objects:
+                if target in visible_objects and self.successes[i] != 1:
                     object_meta = [o for o in objects if o['objectType'] == target]
                     assert len(object_meta) == 1
                     object_meta = object_meta[0]
                     pos = object_meta['position']
                     distance2agent = self.cal_distance(pos, agent_pos)
-                    if distance2agent < self.distances[i] and self.done_each_action[i] != 1:
+                    if distance2agent < self.distances[i] and all(self.done_each_action[:i]):
                         # if we are getting closer to the object \and
                         # the object is not "done"(consider by the agent) yet.
                         reward = 0
                         self.distances[i] = distance2agent
 
-        if action['action'] in [DONE_TOMATO, DONE_BOWL]:
+        if action['action'] in [PICKUP_OBJECT, COOK]:
 
-            done_id = [DONE_TOMATO, DONE_BOWL].index(action['action'])
-            if self.successes[done_id] != 1:
+            done_action_id = [PICKUP_OBJECT, COOK].index(action['action'])
+            self.done_each_action[done_action_id] += 1
 
+            if done_action_id == 0:
+                # if we 'picked' tomato
+                if 'objectId' in action:
+                    picked_obj = action['objectId']
+                    if picked_obj.lower() != 'tomato':
+                        # if not successful, penalize
+                        reward += STEP_PENALTY * 2
+
+            elif done_action_id == 1:
+                # if we 'cook' tomato
+                if self.done_each_action[0] != 0:
+                    reward += STEP_PENALTY * 2
+                elif not action_was_successful:
+                    reward += STEP_PENALTY * 200
+
+            if self.successes[done_action_id] != 1:
                 objects = self._env.last_event.metadata['objects']
                 visible_objects = [o['objectType'] for o in objects if o['visible']]
-                if self.target[done_id] in visible_objects:
+                if 'microwave' in visible_objects:
                     reward += SUCCESS_REWARD
-                    self.successes[done_id] = 1
+                    self.successes[done_action_id] = 1
                     self.success = all(self.successes)
-                if self.args.bonus:
-                    if self.success:
-                        reward += GOAL_SUCCESS_REWARD
 
         all_done = all(self.done_each_action)
         return reward, all_done, action_was_successful
@@ -137,7 +150,7 @@ class Episode:
             self._env.reset(scene)
 
         # For now, single target.BowlTomato
-        self.target = ['Tomato']
+        self.target = ['Tomato', 'Microwave']
         self.success = False
         self.done_each_action = [0 for _ in self.done_each_action]
         self.successes = [0 for _ in self.successes]
